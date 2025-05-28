d887517015ac7212106887fb021f29a4f8610c5e
havidrosi05 <havidrosi05@gmail.com>
2025-05-29 00:04:49 +0700

    feat : db deploy

--- config/db.js
+++ config/db.js
@@ -1,15 +1,32 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'campus_scholarship'
+// const db = mysql.createConnection({
+//     host : 'localhost',
+//     user : 'root',
+//     password : '',
+//     database : 'campus_scholarship'
 
+// });
+
+// db.connect((err) => {
+//     if (err) throw err;
+//     console.log("db success connected")
+// })
+// module.exports = db;
+
+const { Pool } = require('pg');
+
+const db = new Pool({
+    host: 'db.xclrmndtpwpnehczcnlm.supabase.co',
+    user: 'postgres',            
+    password: 'campus_scholarship123', 
+    database: 'postgres',        
+    port: 5432                    // Port untuk PostgreSQL
 });
 
 db.connect((err) => {
     if (err) throw err;
-    console.log("db success connected")
-})
+    console.log("db success connected");
+});
+
 module.exports = db;
